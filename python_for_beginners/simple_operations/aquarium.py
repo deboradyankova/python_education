5846102f13161fb04d1e8d1450d00a5d3c30a4f5
--unset <debora.dyankova@yahoo.com>
2021-10-03 00:32:25 +0300

    Removed comments from the aquarium problem

--- python_for_beginners/simple_operations/aquarium.py
+++ python_for_beginners/simple_operations/aquarium.py
@@ -6,19 +6,13 @@ width = int(input('width in cm:'))
 height = int(input('height in cm:'))
 used_vol_of_items = float(input('used vol of items %')) / CONVERTER_TO_COEF_FOR_MULTIPLICATION
 
-a = 1
-vol_in_l = (length * height * width) / CONVERTER_TO_LITERS
-used_vol_of_items = vol_in_l * used_vol_of_items
-needed_wat = vol_in_l - used_vol_of_items
+volume = length * width * height
+volume_in_liters = volume / CONVERTER_TO_LITERS
+items_vol_in_liters = volume_in_liters * used_vol_of_items
 
-print(f'{needed_wat:.3f}')
-# volume = length * width * height
-# volume_in_liters = volume / CONVERTER_TO_LITERS
-# items_vol_in_liters = volume_in_liters * used_vol_of_items
-#
-# needed_water = volume_in_liters - items_vol_in_liters
-#
-# print(f'{needed_water:.3f}')
+needed_water = volume_in_liters - items_vol_in_liters
+
+print(f'{needed_water:.3f}')
 
 
 
